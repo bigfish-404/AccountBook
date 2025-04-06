831bb86a9cf9bb2d36cf4791bc48e8d1306d093e
bigfish <bigfish.alert@gmail.com>
2025-04-06 23:12:43 +0900

    accountbook

--- accountBook/src/main/java/com/example/accountbook/controller/RegisterController.java
+++ accountBook/src/main/java/com/example/accountbook/controller/RegisterController.java
@@ -5,14 +5,12 @@ import com.example.accountbook.service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/admin")
 public class RegisterController {
 
     @Autowired
