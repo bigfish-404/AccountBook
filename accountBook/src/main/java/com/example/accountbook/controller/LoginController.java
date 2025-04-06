2fa88350982cd80772015f9e4d1c93e2cbd9c205
bigfish <bigfish.alert@gmail.com>
2025-04-06 20:47:04 +0900

    Revert "noone"
    This reverts commit 27654a61b682715f31e0fdc2a4181f2a86a580c0.

--- accountBook/src/main/java/com/example/accountbook/controller/LoginController.java
+++ accountBook/src/main/java/com/example/accountbook/controller/LoginController.java
@@ -1,26 +1,18 @@
 package com.example.accountbook.controller;
 
-
-import com.example.accountbook.service.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.accountbook.entity.LoginRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
+@RequestMapping("/api")
 public class LoginController {
 
-    @Autowired
-    private LoginService loginService;
-
     @PostMapping("/login")
-    public Map<String, Object> login(@RequestBody Map<String, String > payload){
-        String username = payload.get("username");
-        String password = payload.get("password");
-
-        return loginService.login(username,password);
-
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest){
+        return null;
     }
 }
